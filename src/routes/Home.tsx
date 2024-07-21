e24bcb9b35b258aa1ac113e5dd5a21b76cfecaa7
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-07-21 22:29:05 +0900

    feat Home Slider

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 import { getMovies, IGetMoviesResult } from '../api'
 import styled from 'styled-components'
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 import { makeImagePath } from '../utils'
 const Wrapper = styled.div`
   background: black;
@@ -35,18 +35,90 @@ const Overview = styled.p`
   font-size: 30px;
   width: 50%;
 `;
+const Slider = styled.div`
+position:relative;
+top:-100px;
+  
+`
+const Row = styled(motion.div)`
+display: grid;
+gap:5px;
+grid-template-columns: repeat(6,1fr);
+margin-bottom: 5px;
+position:absolute;
+width: 100%;
+`
+const Box = styled(motion.div) <{ bgPhoto: string }>`
+background-color: white;
+height: 200px;
+background-image: url(${(props) => props.bgPhoto});
+background-size: cover;
+background-position: center center;
+`
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 5
+  },
+  visible: {
+    x: 0
+  },
+  exit: {
+    x: -window.outerWidth - 5
+  }
+}
 
+const offset = 6;
 
 function Home() {
-  const {data,isLoading}=useQuery<IGetMoviesResult>(['movies','nowPlaying'],getMovies)
+  const { data, isLoading } = useQuery<IGetMoviesResult>(['movies', 'nowPlaying'], getMovies)
+  const [index, setIndex] = useState(0)
+  const [leaving, setLeaving] = useState(false);
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      setLeaving(true);
+      const totalMovies = data.results.length - 1; // banner item -1
+      const maxIndex = Math.floor(totalMovies / offset) - 1 // page from 0
+      setIndex((prev) => prev === maxIndex ? 0 : prev + 1)
+    }
+  }
+  const toggleLeaving = () => {
+    setLeaving((prev) => !prev)
+  }
   return (
     <Wrapper >
-      { isLoading?<Loader>Loading..</Loader>:(
+      {isLoading ? <Loader>Loading..</Loader> : (
         <>
-        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path||"")}>
-        <Title>{data?.results[0].title}</Title>
-        <Overview>{data?.results[0].overview}</Overview>
-        </Banner>
+          <Banner
+            onClick={increaseIndex}
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+          <Slider>
+            <AnimatePresence
+              initial={false}
+              onExitComplete={toggleLeaving}>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results.slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map(movie =>
+                    <Box
+                      key={movie.id}
+                      bgPhoto={makeImagePath(movie.backdrop_path,"w500")}
+                    />)}
+              </Row>
+            </AnimatePresence>
+
+
+          </Slider>
         </>
       )}
     </Wrapper>
