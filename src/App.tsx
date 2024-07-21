e4590353e852d54352934b4af6f3641ba32b703b
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2024-07-21 12:03:38 +0900

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from './routes/Home';
+import Tv from './routes/Tv';
+import Search from './routes/Search';
 
 
 function App() {
-  return null
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        {/* <Route path="/">
+          <Home />
+        </Route> */}
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
